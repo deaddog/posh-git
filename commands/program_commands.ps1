8a8a95ffb1d7882ca22c5f32de0e4ce85650de9b
deaddog <mikkelslsl@gmail.com>
2014-06-26 21:59:02 +0200

    Created command that opens files directly in sublime text.

--- commands/program_commands.ps1
+++ commands/program_commands.ps1
@@ -14,6 +14,12 @@ function StartNotepadPlusPlus([string]$arg1)
     Write-Host "Starting Notepad++: ""$arg1"""
 	Start-Process "C:\Program Files (x86)\Notepad++\notepad++.exe" $arg1
 }
+function StartSublimeText([string]$arg1)
+{
+    Write-Host "Sublime Text 2: ""$arg1"""
+	Start-Process "C:\Program Files\Sublime Text 2\sublime_text.exe" $arg1
+}
 
 Set-Alias -Name npp -Value StartNotepadPlusPlus
+Set-Alias -Name sub -Value StartSublimeText
 Set-Alias -Name ex -Value OpenExplorer
