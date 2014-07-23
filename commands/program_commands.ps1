728528d91774ebde93f4e89637af7fa8a3f53217
deaddog <mikkelslsl@gmail.com>
2014-07-23 17:33:08 +0200

    Notepad++ and Sublime Text 2 will start without a file argument.

--- commands/program_commands.ps1
+++ commands/program_commands.ps1
@@ -11,13 +11,35 @@
 }
 function StartNotepadPlusPlus([string]$arg1)
 {
-    Write-Host "Starting Notepad++: ""$arg1"""
-	Start-Process "C:\Program Files (x86)\Notepad++\notepad++.exe" $arg1
+    Write-Host "Starting " -ForegroundColor "DarkGray" -NoNewline
+    Write-Host "Notepad++" -ForegroundColor "Green" -NoNewline
+
+    if($arg1 -eq "")
+    {
+        Start-Process "C:\Program Files (x86)\Notepad++\notepad++.exe"
+    }
+    else
+    {
+        Write-Host ": ""$arg1""" -NoNewline
+        Start-Process "C:\Program Files (x86)\Notepad++\notepad++.exe" $arg1
+    }
+    Write-Host ""
 }
 function StartSublimeText([string]$arg1)
 {
-    Write-Host "Sublime Text 2: ""$arg1"""
-	Start-Process "C:\Program Files\Sublime Text 2\sublime_text.exe" $arg1
+    Write-Host "Starting " -ForegroundColor "DarkGray" -NoNewline
+    Write-Host "Sublime Text 2" -ForegroundColor "Magenta" -NoNewline
+
+    if($arg1 -eq "")
+    {
+        Start-Process "C:\Program Files\Sublime Text 2\sublime_text.exe"
+    }
+    else
+    {
+        Write-Host ": ""$arg1""" -NoNewline
+        Start-Process "C:\Program Files\Sublime Text 2\sublime_text.exe" $arg1
+    }
+    Write-Host ""
 }
 
 Set-Alias -Name npp -Value StartNotepadPlusPlus
