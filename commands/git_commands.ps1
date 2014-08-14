f18077a97cad390504fbeb79b343f68b1572795c
deaddog <mikkelslsl@gmail.com>
2014-08-14 11:15:54 +0200

    Removes the 'gpush', 'gpull' and 'gsync' alias' as they were not used and possibly malicious to use.

--- commands/git_commands.ps1
+++ commands/git_commands.ps1
@@ -25,16 +25,6 @@ function RunGitLogSimple()
 function RunGitDiff([string]$file) {
     git diff --word-diff=color -w $file
 }
-function RunGitPush { 
-	git push origin master
-}
-function RunGitPull { 
-	git pull origin master
-}
-function RunGitSync { 
-	git pull origin master
-    git push origin master
-}
 function RunGitStatusWithClear {
 	clear;
 	RunGitStatus;
@@ -56,9 +46,6 @@ Set-Alias -Name gstac -Value RunGitStatusWithClear
 Set-Alias -Name giff -Value RunGitDiff
 Set-Alias -Name glog -Value RunGitLog
 Set-Alias -Name glo -Value RunGitLogSimple
-Set-Alias -Name gpush -Value RunGitPush
-Set-Alias -Name gpull -Value RunGitPull
-Set-Alias -Name gsync -Value RunGitSync
 Set-Alias -Name gamend -Value RunGitAmend
 Set-Alias -Name gic -Value RunGitCommit
 Set-Alias -Name gdelnew -Value RunGitDeleteUnaddedFiles
