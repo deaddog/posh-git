7123096deb10fdedf960d98bd18fc1456b0bb8df
deaddog <mikkelslsl@gmail.com>
2014-09-18 12:15:55 +0200

    Altered the PS prompt to use a newline after the current path. Included current path in the git prompt.

--- profile.example.ps1
+++ profile.example.ps1
@@ -14,14 +14,21 @@ function global:prompt {
 
     # Reset color, which can be messed up by Enable-GitColors
     $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
-
+    
+    Write-Host("`r`n") -nonewline
     Write-VcsStatus
 
-	if(-Not $Global:GitStatus)
+    if(-Not $Global:GitStatus)
     {
-		Write-Host($pwd.ProviderPath) -nonewline
-	}
-	
+        Write-Host($pwd.ProviderPath + "\") -nonewline
+        Write-Host("`r`n") -nonewline
+    }
+    else
+    {
+        Write-Host(" " + $pwd.ProviderPath + "\") -nonewline -foregroundcolor "yellow"
+        Write-Host("`r`n") -nonewline
+    }
+
     $global:LASTEXITCODE = $realLASTEXITCODE
     return "> "
 }
