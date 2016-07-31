edb7c9c4b50681bbfe73532bf8370fb2a5b5de5c
deaddog <mikkelslsl@gmail.com>
2016-07-31 10:28:06 +0200

    Included the function to open a visual studio solution.

--- commands/program_commands.ps1
+++ commands/program_commands.ps1
@@ -45,6 +45,35 @@ function ClearLatexDirectory()
 {
     ls -r | Where {$_ -match "\.(aux|bbl|bak|bit|blg|dvi|lof|log|lot|glo|ilg|ind|out|pre|synctex.gz|tdo|thm|toc|run.xml|)$"} | rm
 }
+function OpenVisualStudioSolution() {
+  $path = (Get-Item .).fullname
+
+  while ($true) {
+    $a = ls $path | Where { $_ -match "\.sln$" } | Select-Object -first 1
+
+    if (!($a -eq $null))
+    {
+      Write-Host "Opening """ -ForegroundColor "DarkGray" -NoNewline
+      Write-Host $a -ForegroundColor "Green" -NoNewline
+      Write-Host """ in Visual Studio" -ForegroundColor "DarkGray"
+
+      #Invoke-Item $a.fullname
+      Start-Process "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" $a.fullname
+      return;
+    }
+    else
+    {
+      $path = (get-item $path).parent
+      if ($path -eq $null) {
+        Write-Host "No solution file found." -ForegroundColor "Red"
+        return;
+      }
+      else {
+        $path = $path.fullname
+      }
+    }
+  }
+}
 
 function BuildRelease()
 {
@@ -54,5 +83,6 @@ function BuildRelease()
 Set-Alias -Name npp -Value StartNotepadPlusPlus
 Set-Alias -Name sub -Value StartSublimeText
 Set-Alias -Name ocd -Value OpenExplorer
+Set-Alias -Name vs -Value OpenVisualStudioSolution
 Set-Alias -Name msbuild -Value "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe"
 Set-Alias -Name msbuildr -Value BuildRelease
